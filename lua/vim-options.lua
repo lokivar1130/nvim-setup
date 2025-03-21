315af9e4755e10c12541684fe622c88126f40d40
lokivar1130 <lokivar1130@gmail.com>
2025-03-21 19:18:31 +0000

    fix neotree with lazyvim config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,8 +9,8 @@ local opts = { noremap = true, silent = true }
 
 -- Open a window to the left (vertical split and move cursor to new split)
 map("n", "<leader>wl", function()
-	vim.cmd("vsplit")
-	vim.cmd("wincmd H") -- Move split to the left
+    vim.cmd("vsplit")
+    vim.cmd("wincmd H") -- Move split to the left
 end, opts)
 
 -- Open a window to the right (vertical split stays on the right)
@@ -18,3 +18,7 @@ map("n", "<leader>wr", ":vsplit<CR>", opts)
 
 -- Close the current window
 map("n", "<leader>wq", ":close<CR>", opts)
+
+local ui = require("utils.ui")
+
+map("n", "<leader>bd", ui.bufremove, { desc = "Delete buffer" })
