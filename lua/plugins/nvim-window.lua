8f17d43d500416857ea797c2966a6fef6df9ba1d
lokivar1130 <lokivar1130@gmail.com>
2025-03-21 02:20:19 +0000

    better window management

---
+++ lua/plugins/nvim-window.lua
@@ -0,0 +1,45 @@
+return {
+
+    "yorickpeterse/nvim-window",
+    config = function()
+        local nvim_window = require("nvim-window")
+        nvim_window.setup({
+            keys = {
+                { "<leader>wj", "<cmd>lua require('nvim-window').pick()<cr>", desc = "nvim-window: Jump to window" },
+            },
+
+            chars = {
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "j",
+                "k",
+                "l",
+                "m",
+                "n",
+                "o",
+                "p",
+                "q",
+                "r",
+                "s",
+                "t",
+                "u",
+                "v",
+                "w",
+                "x",
+                "y",
+                "z",
+            },
+            config = true,
+        })
+        vim.keymap.set("n", "<leader>wj", function()
+            require("nvim-window").pick()
+        end, { desc = "nvim-window: Jump to window", noremap = true, silent = true })
+    end,
+}
